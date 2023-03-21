18ff596a87436aeada1cb6cc663b44783a614e61
N4YST <shaun05270439@gmail.com>
2023-03-21 16:13:05 +0800

    first commit

---
+++ src/com/nayst/fruit/Add.java
@@ -0,0 +1,17 @@
+package com.nayst.fruit;
+
+import jakarta.servlet.annotation.WebServlet;
+
+/**
+ * ClassName: add
+ * Package: com.nayst.fruit
+ * Description:
+ *
+ * @Author: Nayst
+ * @Create: 2023/3/21 15:38
+ * @Version: 1.0
+ */
+@WebServlet("/add")
+public class Add {
+    //课件里的代码我没有，懒得找了
+}
--- src/com/nayst/servlets/Thymeleaf.java
+++ src/com/nayst/servlets/Thymeleaf.java
@@ -1,13 +1,18 @@
 package com.nayst.servlets;
 
+import jakarta.servlet.annotation.WebServlet;
+
 /**
  * ClassName: Thymeleaf
  * Package: com.nayst.servlets
  * Description:
- *
+ *      Servlet从3.0开始支持注解方式注册
  * @Author: Nayst
  * @Create: 2023/3/20 17:59
  * @Version: 1.0
  */
+
+@WebServlet("/thymeleaf")
 public class Thymeleaf {
+    // 课件上的代码我没有，懒得找了
 }
